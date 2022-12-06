ced4f5e69fd029640b2a208cfa86329a2e68e704
Audran Malosse <audran.malosse@etu.univ-lyon1.fr>
2022-12-06 09:55:34 +0100

    Ajout Etudiant Boursier et Interface Utile

--- saejava_blevin_malosse_spitz/src/Personne/Etudiant.java
+++ saejava_blevin_malosse_spitz/src/Personne/Etudiant.java
@@ -8,10 +8,38 @@ package Personne;
  *
  * @author audranmalosse
  */
-public class Etudiant extends Personne {
+public class Etudiant extends Personne implements Util {
     private String numEtudiant;
+    
+    
     public Etudiant (String Nom, String Prenom){
         super(Nom, Prenom);
         this.numEtudiant = "E" + String.valueOf(getNumPersonne());
+        super.incrTab(this);
+        
+    }
+    public boolean anneeValidee () {
+         if (getAbsNonJustif()>=5)
+            return true;
+        else
+            return false;
+    }
+
+    @Override
+    public boolean equals(Personne P) {
+        return super.equals(P);
+    }
+
+    @Override
+    public String toString() {
+        return super.toString();
+    }
+    /**Méthode abstraite qui verifie le nombre d'absence*/
+    @Override
+    public void verifAbs(){
+        if (super.getAbsNonJustif()>=5)
+            System.out.print("L'etudiant a trop d'absences pour valider son année");
+        else
+            System.out.print("L'etudiant a n'a pas de souci d'absence pour valider son année");
     }
 }
--- saejava_blevin_malosse_spitz/src/Personne/Personne.java
+++ saejava_blevin_malosse_spitz/src/Personne/Personne.java
@@ -8,14 +8,17 @@ package Personne;
  *
  * @author audranmalosse
  */
-public abstract class Personne {
+public abstract class Personne implements Util{
+
     // attributs 
     private int numPersonne=0;
     private int absJustif= 0;
     private int absNonJustif =0;
     private String nom;
     private String prenom;
-    public static int cpt = 0;
+    private static int cpt = 0;
+    private static Personne []registre = new Personne[0];
+    
     
     /**Constructeur qui utilise les setters avec Nom et Prenom comme argument*/
     public Personne (String Nom, String Prenom){
@@ -46,23 +49,57 @@ public abstract class Personne {
     }
     //setter
     public void setNom(String Nom){ this.nom = verifVide(Nom);}
+    
     public void setPrenom(String Prenom){this.prenom = verifVide(Prenom);}
     
+    /**addAbsNonJustif ajoute une absence injustifiée*/
+    public void addAbsNonJustif(){this.absNonJustif++;}
+    
     /** addAbsNonJustif ajoute la valeurs transmise en paramètre au nombre d'absences déjà enregistrées*/
-    public void addAbsNonJustif(int Nombre){this.absNonJustif+=Nombre;}
+    public void addAbsNonJustif(int Nombre){this.absNonJustif+=Math.abs(Nombre);}
+    
+    /** addAbsJustif transforme une absence injustifiée en absence justifiée 
+    si il y en a au moins une.*/
+    public void addAbsJustif (){
+        if (absNonJustif >= 1){
+            this.absJustif += 1;
+        }
+        else
+            System.out.println("ERREUR: Il n'y a pas assez d'absences non justifiée");
+    }
     
     /** addAbsJustif transforme le nombre d'absence injustifiée, transmise en paramètre, en absence justifiée*/
     public void addAbsJustif (int Nombre){
         
-        if (absNonJustif>=Nombre)
-            this.absJustif-=Nombre;
+        if (absNonJustif>=Math.abs(Nombre)){
+            this.absJustif-=Math.abs(Nombre);
+            this.absNonJustif -= 1;
+        }
         else
             System.out.println("ERREUR: Il n'y a pas assez d'absences non justifiée"); 
     }
-    public boolean equals (Object obj){
+    /** Test d'égalité de deux peronnes*/
+    public boolean equals (Personne obj){
         if (!(obj instanceof Personne))
             return false;
         Personne p = (Personne) obj;
         return this.nom.equals(p.nom) && this.prenom.equals(p.prenom) && this.numPersonne == p.numPersonne;
     }
+    
+    /** Méthode d'incrémentation du tableau*/
+    public void incrTab (Personne P){
+        Personne[] newTab = new Personne[Personne.registre.length + 1];
+        for(int i = 0; i< newTab.length - 1; i++)
+        newTab[i] = Personne.registre[i];
+        newTab[newTab.length-1] = P;
+        Personne.registre = newTab;
+    }
+    
+    public String toString(){
+        return "Nom: " + this.nom + "Prenom: " + this.prenom + "id: " + this.numPersonne;
+    }
+    
+    /**Méthode abstraite qui verifie le nombre d'absence*/
+    public abstract void verifAbs();
+    
 }
