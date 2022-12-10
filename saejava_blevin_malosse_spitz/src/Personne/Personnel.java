66ec2211689a9a28cfdde538035a872dd4be7143
Audran Malosse <audran.malosse@etu.univ-lyon1.fr>
2022-12-10 09:06:53 +0100

    Avancement Class Personnel avec la prime

--- saejava_blevin_malosse_spitz/src/Personne/Personnel.java
+++ saejava_blevin_malosse_spitz/src/Personne/Personnel.java
@@ -10,9 +10,8 @@ package Personne;
  */
 public abstract class Personnel extends Personne {
     
-    private double salaireFixe;
     private static double primeAnnuelle;
-    private boolean primeDispo = false;
+    private double salaireFixe;
     private double salaireTotalMensuel;
     private double salaireTotalAnnuel;
     
@@ -30,42 +29,33 @@ public abstract class Personnel extends Personne {
     //getter
     public double getSalaireFixe(){return this.salaireFixe;}
     public static double getPrimeAnnuelle(){return Personnel.primeAnnuelle;}
-    public boolean getPrimeDispo(){return this.primeDispo;}
     
     //setter
     public void setSalaireFixe(double Val){this.salaireFixe = Math.abs(Val);}
     public static void setPrimeAnnuelle(double Val){Personnel.primeAnnuelle = Math.abs(Val);}
-    public void setPrimeDispo(boolean Etat){this.primeDispo = Etat;}
     
     /** Test d'égalité de deux peronnes*/
     public boolean equals (Personnel obj){
         if (!(obj instanceof Personnel))
             return false;
         Personnel p = (Personnel) obj;
-        return super.equals(obj) && this.salaireFixe == p.salaireFixe && this.primeDispo== p.primeDispo;    
+        return super.equals(obj) && this.salaireFixe == p.salaireFixe;    
     }
     
     /**Redéfinition de toString*/
     public String toString(){
         return super.toString() + "Le salaire fixe est de : " + salaireFixe + 
-                "\nLa personne est éligible à la prime : " + primeDispo + 
                 "\nLe salaire mensuel est de : " + salaireTotalMensuel +
                 "\nLe salaire annuel total est de :" + salaireTotalAnnuel;
     }
     
     /**Défini le salaire mensuel du personnel avec la gestion de la disponibilitée de la prime ou non*/
     public void defSalaireMensuel(){
-        if (primeDispo == true)
-            this.salaireTotalMensuel = this.getSalaireFixe() + Personnel.getPrimeAnnuelle();
-        else
             this.salaireTotalAnnuel = this.getSalaireFixe();
     }
     
     /**Défini le salaire annuel du personnel avec la gestion de la disponibilitée de la prime ou non*/
     public void defSalaireAnnuel(){
-        if (primeDispo == true)
-            this.salaireTotalAnnuel = (this.getSalaireFixe() + Personnel.getPrimeAnnuelle())*12;
-        else
             this.salaireTotalAnnuel = this.getSalaireFixe()*12;
     }
 }
--- saejava_blevin_malosse_spitz/src/Personne/PersonnelAdministratif.java
+++ saejava_blevin_malosse_spitz/src/Personne/PersonnelAdministratif.java
@@ -10,4 +10,36 @@ package Personne;
  */
 public class PersonnelAdministratif extends Personnel {
     
+    private boolean primeDispo = false;
+    
+    /**Constructeur qui donne la possibilitée d'ajouter la disponibilitée de la prime*/
+    public PersonnelAdministratif (String Nom, String Prenom, Double SalaireFixe, boolean PrimeDispo) {
+        super(Nom, Prenom, SalaireFixe);
+        if(PrimeAnnuelle == true || PrimeAnnuelle == false)
+
+
+    }
+
+    /**Méthode qui permet de donner l'acces à la prime ou de le retirer*/
+    public void setAccesPrime(boolean Acces) {
+        if (Acces == true || Acces == false)
+            this.primeDispo = Acces;
+        else
+            this.primeDispo = false;
+    }
+    
+    /** Test d'égalité de deux peronnes*/
+    public boolean equals (PersonnelAdministratif obj){
+        if (!(obj instanceof PersonnelAdministratif))
+            return false;
+        PersonnelAdministratif p = (PersonnelAdministratif) obj;
+        return super.equals(obj) && this.primeDispo == p.primeDispo;    
+    }
+    
+    /**Redéfinition de toString*/
+    public String toString(){
+        return super.toString() + "\nCette personne a accès à la prime : " + this.primeDispo;
+    }
+    
+    
 }
