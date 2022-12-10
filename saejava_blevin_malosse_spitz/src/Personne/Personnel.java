68df431b921b38309f7d5a5ebbc3ca760e43f60f
Audran Malosse <audran.malosse@etu.univ-lyon1.fr>
2022-12-10 10:36:33 +0100

    Passage des arguments de la prime dans la class Personnel

--- saejava_blevin_malosse_spitz/src/Personne/Personnel.java
+++ saejava_blevin_malosse_spitz/src/Personne/Personnel.java
@@ -11,6 +11,8 @@ package Personne;
 public abstract class Personnel extends Personne {
     
     private static double primeAnnuelle;
+    private boolean obtentionPrime = false;
+    private boolean droitPrime = false;
     private double salaireFixe;
     private double salaireTotalMensuel;
     private double salaireTotalAnnuel;
@@ -29,10 +31,14 @@ public abstract class Personnel extends Personne {
     //getter
     public double getSalaireFixe(){return this.salaireFixe;}
     public static double getPrimeAnnuelle(){return Personnel.primeAnnuelle;}
+    public boolean getObtensionPrime() {return this.obtentionPrime;}
+    public boolean getDroitPrime() {return this.droitPrime;}
     
     //setter
     public void setSalaireFixe(double Val){this.salaireFixe = Math.abs(Val);}
     public static void setPrimeAnnuelle(double Val){Personnel.primeAnnuelle = Math.abs(Val);}
+    public void setObtentionPrime(boolean Bool){this.obtentionPrime = verifBool(Bool);}
+    public void setDroitPrime(boolean Bool){this.droitPrime = verifBool(Bool);}
     
     /** Test d'égalité de deux peronnes*/
     public boolean equals (Personnel obj){
@@ -44,18 +50,44 @@ public abstract class Personnel extends Personne {
     
     /**Redéfinition de toString*/
     public String toString(){
-        return super.toString() + "Le salaire fixe est de : " + salaireFixe + 
-                "\nLe salaire mensuel est de : " + salaireTotalMensuel +
-                "\nLe salaire annuel total est de :" + salaireTotalAnnuel;
+        return super.toString() + "\nLe salaire fixe est de : " + salaireFixe + 
+            "\nLe salaire mensuel est de : " + salaireTotalMensuel +
+            "\nLe salaire annuel total est de :" + salaireTotalAnnuel;
     }
     
-    /**Défini le salaire mensuel du personnel avec la gestion de la disponibilitée de la prime ou non*/
+    /**Permet de verifier si l'argument transmis est bien un booléen*/
+    public boolean verifBool(boolean Bool){
+        if (Bool == true || Bool == false)
+            return Bool;
+        else
+            return false;
+    }
+    /**Permet de donner l'obtenstion ou non à la Prime*/
+    public void setaccesPrime (boolean Bool){
+        this.obtentionPrime = verifBool(Bool);
+    }
+    /**Permet de donner le droit ou non à la Prime*/
+    public void setdroitPrime (boolean Bool){
+        this.droitPrime = verifBool(Bool);
+    }
+    
+    /**Défini le salaire mensuel du personnel*/
     public void defSalaireMensuel(){
-            this.salaireTotalAnnuel = this.getSalaireFixe();
+        this.salaireTotalMensuel = this.getSalaireFixe();
     }
     
-    /**Défini le salaire annuel du personnel avec la gestion de la disponibilitée de la prime ou non*/
+    /**Défini le salaire annuel du personnel*/
     public void defSalaireAnnuel(){
-            this.salaireTotalAnnuel = this.getSalaireFixe()*12;
+        this.salaireTotalAnnuel = this.getSalaireFixe()*12;
+    }
+    
+    /**Méthode qui permet d'augmenter le salaire mensuel de la valeure transmise*/
+    public void addAtSalaireMensuel(double Val){
+        this.salaireTotalMensuel += Val;
+    }
+    
+    /**Méthode qui permet d'augmenter le salaire annuel de la valeure transmise*/
+    public void addAtSalaireAnnuel(double Val){
+        this.salaireTotalAnnuel += Val;
     }
 }
