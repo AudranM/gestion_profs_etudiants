62f08094545112be5b7eed3f958598a7c35efecf
Fabio <fabioblevin@hotmail.fr>
2022-12-12 23:12:18 +0100

    Class Application ajout et suppresion registre

--- saejava_blevin_malosse_spitz/src/Personne/Application.java
+++ saejava_blevin_malosse_spitz/src/Personne/Application.java
@@ -4,10 +4,114 @@
  */
 package Personne;
 
+import static Personne.Console.*;
+
 /**
  *
  * @author audranmalosse
  */
 public class Application {
-    
+
+    // Menu principale
+    public static void menu() {
+        int choix = -1;
+        System.out.println("\nMenu principale");
+        System.out.println("\nMerci de faire votre choix");
+        System.out.println("\n0 : Arrêter le programme");
+        System.out.println("\n1 : Ajouter au registre");
+        System.out.println("\n2 : Effacer du registre");
+        System.out.println("\n3 : Les etudiants avec plus de 5 absence injustifié");
+        System.out.println("\n4 : Liste des salariés qui ont une retenu sur salaire");
+        System.out.println("\n5 : Voir les informations d'une personne dans le registre");
+        choix = Clavier.lireInt();
+
+        switch (choix) {
+            case 0:
+                quitterLeProgramme();
+                break;
+            case 1:
+                ajoutPersonne();
+                break;
+            case 2:
+                suprimePersonne();
+                break;
+            case 3:
+                ajoutPersonne();
+                break;
+            case 4:
+                ajoutPersonne();
+                break;
+            case 5:
+                ajoutPersonne();
+                break;
+            default:
+                menu();
+                break;
+        }
+    }
+
+    public static void ajoutPersonne() {
+        int choix = -1;
+        System.out.println("\nMerci de faire votre choix");
+        System.out.println("\n0 : Arrêter le programme");
+        System.out.println("\n1 : retourner au menu principale");
+        System.out.println("\n2 : Ajouter un etudiant");
+        System.out.println("\n3 : Ajouter un etudiant boursier");
+        System.out.println("\n4 : Ajouter un enseignant titulaire");
+        System.out.println("\n5 : Ajouter un enseignant vacataire");
+        System.out.println("\n6 : Ajouter un personnel administratif");
+        choix = Clavier.lireInt();
+
+        switch (choix) {
+            case 0:
+                quitterLeProgramme();
+                break;
+            case 1:
+                menu();
+                break;
+            case 2:
+                creatEtudiant();
+                break;
+            case 3:
+                creatEtudiantBoursier();
+                break;
+            case 4:
+                creatPersonnelEnseignantTitulaire();
+                break;
+            case 5:
+                creatPersonnelEnseignantVacataire();
+                break;
+            case 6:
+                creatPersonnelAdministratif();
+                break;
+            default:
+                menu();
+                break;
+
+        }
+    }
+
+    public static void suprimePersonne() {
+        int choix = -1;
+        System.out.println("\nMenu principale");
+        System.out.println("\nMerci de faire votre choix");
+        System.out.println("\n0 : Menu principale");
+        System.out.println("\n1 : Effacer un element du registre");
+        System.out.println("\n2 : Voir le registre etudiant");
+
+        switch (choix) {
+            case 0:
+                menu();
+                break;
+            case 1:
+                effacerUnElement();
+                break;
+            case 2:
+                ;
+                break;
+            case 3:
+                ;
+                break;
+        }
+    }
 }
--- saejava_blevin_malosse_spitz/src/Personne/Console.java
+++ saejava_blevin_malosse_spitz/src/Personne/Console.java
@@ -4,39 +4,98 @@
  */
 package Personne;
 
+import static Personne.Personne.*;
+
 /**
  *
- * @author audranmalosse
+ * @author fabio
  */
 public class Console {
-    public static void creatEtudiant (){
-        System.out.println("Saisir le Nom");
-        String x = Clavier.lireString();
-        System.out.println("Saisir le Prénom");
-        String y = Clavier.lireString(); 
-        Personne Personne = new Etudiant (x,y);
+    /**
+     * Méthode pour supprimer un element de Arraylist dans la classe Personne
+     * @return 
+     */
+    public static String effacerUnElement() {
+        System.out.println("indiquer le Numero de la personne à supprimer du registre : ");
+        int idASupprimer = Clavier.lireInt();
+        if (idASupprimer < 0 || idASupprimer > tailleRegistre()) {
+            return "Le numero indiqué n'est pas dans le registre";
+        }
+        if (recupIndiceRegistre(idASupprimer) < 0) {
+            return "Le numéro de la personne n'a pas été trouvé.";
+        }
+        supprimePersonne(recupIndiceRegistre(idASupprimer));
+        return "le numéro "+idASupprimer+ "a bien été supprimé";
     }
-    
-    public static void creatPersonnelAdministratif (){
-        System.out.println("Saisir le Nom");
-        String x = Clavier.lireString();
-        System.out.println("Saisir le Prénom");
-        String y = Clavier.lireString(); 
-        Personne Personne = new PersonnelAdministratif (x,y);
+
+    /**
+     * Méthode pour quitter le programme
+     */
+    public static void quitterLeProgramme() {
+        System.out.println("Merci d'avoir utilisé notre programme");
+        System.exit(1);
     }
-    
-    public static void creatPersonnelEnseignantTitulaire (){
+
+    /**
+     * Méthode de creation d'un etudiant
+     */
+    public static void creatEtudiant() {
         System.out.println("Saisir le Nom");
-        String x = Clavier.lireString();
+        String nom = Clavier.lireString();
         System.out.println("Saisir le Prénom");
-        String y = Clavier.lireString(); 
-        Personne Personne = new PersonnelEnseignantTitulaire (x,y);
+        String prenom = Clavier.lireString();
+        Personne Personne = new Etudiant(nom, prenom);
     }
-    public static void creatPersonnelEnseignantVacataire (){
+
+    /**
+     * Méthode de creation d'un etudiant boursier
+     */
+    public static void creatEtudiantBoursier() {
         System.out.println("Saisir le Nom");
-        String x = Clavier.lireString();
+        String nom = Clavier.lireString();
         System.out.println("Saisir le Prénom");
-        String y = Clavier.lireString(); 
-        Personne Personne = new PersonnelEnseignantVacataire (x,y);
+        String prenom = Clavier.lireString();
+        System.out.println("Saisir le montant de la bourse");
+        double bourse = Clavier.lireDouble();
+        Personne Personne = new EtudiantBoursier(nom, prenom, bourse);
+    }
+
+    /**
+     * Méthode de creation d'un personnel administratif
+     */
+    public static void creatPersonnelAdministratif() {
+        System.out.println("Saisir le Nom");
+        String nom = Clavier.lireString();
+        System.out.println("Saisir le Prénom");
+        String prenom = Clavier.lireString();
+        System.out.println("Saisir le salaire fixe");
+        double salaireFixe = Clavier.lireDouble();
+        Personne Personne = new PersonnelAdministratif(nom, prenom, salaireFixe);
+    }
+
+    /**
+     * Méthode de creation d'un personnel enseignant titulaire
+     */
+    public static void creatPersonnelEnseignantTitulaire() {
+        System.out.println("Saisir le Nom");
+        String nom = Clavier.lireString();
+        System.out.println("Saisir le Prénom");
+        String prenom = Clavier.lireString();
+        System.out.println("Saisir le salaire fixe");
+        double tarifHeureSup = Clavier.lireDouble();
+        Personne Personne = new PersonnelEnseignantTitulaire(nom, prenom, tarifHeureSup);
+    }
+
+    // a finir
+    /**
+     * Méthode de creation d'un personnel enseignant vacataire
+     */
+    public static void creatPersonnelEnseignantVacataire() {
+        System.out.println("Saisir le Nom");
+        String nom = Clavier.lireString();
+        System.out.println("Saisir le Prénom");
+        String prenom = Clavier.lireString();
+        System.out.println("Saisir le salaire fixe");
+        Personne Personne = new PersonnelEnseignantVacataire(nom, prenom);
     }
 }
