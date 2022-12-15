ac3ce2dc281d1431dcf4a2dfa5dec472dd3bd811
Audran Malosse <audran.malosse@etu.univ-lyon1.fr>
2022-12-15 21:35:38 +0100

    Création de l'interface ISalaire avec SalaireAdministrateur, Titulaire, Vacataire

---
+++ saejava_blevin_malosse_spitz/src/Personne/ISalaire.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Personne;
+
+/**
+ *
+ * @author audranmalosse
+ */
+public interface ISalaire {
+    public double getSalaireMensuel();
+    public void setSalaireMensuel(double val);
+}
---
+++ saejava_blevin_malosse_spitz/src/Personne/SalaireAdministrateur.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Personne;
+
+
+/**
+ *
+ * @author audranmalosse
+ */
+public class SalaireAdministrateur  {
+    private double salaireFixe;
+    
+    public double getSalaireMensuel(){return this.salaireFixe;}
+    public void setSalaireMensuel(double val){this.salaireFixe = val;}
+}
